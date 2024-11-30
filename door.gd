c33cb90ea381487df2c580ec8d0fb35c065af178
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-11-30 10:55:33 +0100

    Make platform move configurable via the editor

--- door.gd
+++ door.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var visuals: AnimatedSprite2D
 @export var button_visuals: AnimatedSprite2D
 @export var connected_plaform: AnimatableBody2D
+@export var move_difference: float = 300
+@export var move_duration: float = 3
 var tween: Tween
 
 func _on_area_entered(area: Area2D) -> void:
@@ -18,5 +20,5 @@ func _on_button_body_entered(body: Node2D) -> void:
 	button_visuals.play("pressed")
 		
 	tween = create_tween().set_loops()
-	tween.tween_property(connected_plaform, "position", Vector2.UP*-300, 3).as_relative()
-	tween.tween_property(connected_plaform, "position", Vector2.UP*300, 3).as_relative()
+	tween.tween_property(connected_plaform, "position", Vector2.UP*-move_difference, move_duration).as_relative()
+	tween.tween_property(connected_plaform, "position", Vector2.UP*move_difference, move_duration).as_relative()
