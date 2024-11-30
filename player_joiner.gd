65922b7280c8187a5188d2c0a34593230fb0521f
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-11-30 08:26:28 +0100

    Load allowed player prefixes from InputMap

--- player_joiner.gd
+++ player_joiner.gd
@@ -2,27 +2,33 @@ class_name PlayerJoiner
 extends Node
 
 @export var player_scene: PackedScene
-@export var maximum_players: int = 6
 @export var colors: Array[Color]
 @export var actions: Array[String] = ["move_left", "move_right", "jump"]
 @export var player_numbers_taken: Array[int] = []
-
+@export var player_prefixes_taken: Array[String] = []
+@export var prefixes: Array
 @export var players_parent: Node2D
 @export var spawn_point: Marker2D
 
+func _ready() -> void:
+	prefixes = InputMap.get_actions().filter(func (a: String): return a.ends_with("jump")).map(func(a: String): return a.replace("jump", ""))
+
 func try_joining(event: InputEvent) -> RigidBody2D:
-	for i in range(maximum_players):
-		if i in player_numbers_taken:
+	for i in prefixes.size():
+		var p = prefixes[i]
+		if p in player_prefixes_taken:
 			continue
-			
-		var prefix = "p%d_" % i
+		
 		for a in actions:
-			var prefixed_action = "%s%s" % [prefix, a]
+			var prefixed_action = "%s%s" % [p, a]
 			if event.is_action(prefixed_action):
 				var player = player_scene.instantiate() as RigidBody2D
-				player.player_prefix = prefix
-				player.color = colors[i]
-				player_numbers_taken.append(i)
+				player.player_prefix = p
+				if i < colors.size():
+					player.color = colors[i]
+				else:
+					player.color = Color.from_hsv((i*75.0/360.0), 1, 1)
+				player_prefixes_taken.append(p)
 				players_parent.add_child(player)
 				player.position = spawn_point.position
 				return player
