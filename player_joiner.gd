ec617a59568ffb6758a2ef089a9e1cd88738ec00
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-05-09 22:00:43 +0200

    Enable split joypad controls

--- player_joiner.gd
+++ player_joiner.gd
@@ -10,31 +10,31 @@ extends Node
 @export var players_parent: Node2D
 @export var spawn_point: Marker2D
 @export var joypad_movement_controls: MovementControls
+@export var right_split_joypad_movement_controls: MovementControls
 @export var device_player_map: Dictionary[int, Array]
 
 func _ready() -> void:
 	Input.joy_connection_changed.connect(_on_joy_connection_changed)
 	
-	for i in 4:
-		var prefix = "p_joy_{id}_".format({"id": i})
-		prefixes.append(prefix)
-		for a in actions:
-			var prefixed_action_name = "{prefix}{name}".format({"prefix": prefix, "name": a})
-			var input_events = joypad_movement_controls.get(a) as Array[InputEvent]
+	#for i in 4:
+		#add_player_input(i, joypad_movement_controls, "joy_left")
+		#add_player_input(i, right_split_joypad_movement_controls, "joy_right")
+		
+func add_player_input(i: int, movement_controls, infix: String):
+	var prefix = "p_{infix}_{id}_".format({"infix": infix, "id": i})
+	prefixes.append(prefix)
+	for a in actions:
+		var prefixed_action_name = "{prefix}{name}".format({"prefix": prefix, "name": a})
+		var input_events = movement_controls.get(a) as Array[InputEvent]
+		
+		if not InputMap.has_action(prefixed_action_name):
+			InputMap.add_action(prefixed_action_name)
 			
-			if not InputMap.has_action(prefixed_action_name):
-				InputMap.add_action(prefixed_action_name)
-				
-			for event in input_events:
-				var specific_event = event.duplicate() as InputEvent
-				specific_event.device = i
-				if not InputMap.action_has_event(prefixed_action_name, specific_event):
-					InputMap.action_add_event(prefixed_action_name, specific_event)
-				
-				
-			print(InputMap.action_get_events(prefixed_action_name))
-				
-			print(prefixes)
+		for event in input_events:
+			var specific_event = event.duplicate() as InputEvent
+			specific_event.device = i
+			if not InputMap.action_has_event(prefixed_action_name, specific_event):
+				InputMap.action_add_event(prefixed_action_name, specific_event)
 
 func try_joining(event: InputEvent) -> RigidBody2D:
 	for i in prefixes.size():
@@ -63,10 +63,16 @@ func try_joining(event: InputEvent) -> RigidBody2D:
 	return null
 
 func _on_joy_connection_changed(device: int,  connected: bool):
-	if not connected and device_player_map.has(device):
-		var device_players = device_player_map[device] as Array[RigidBody2D]
-		for player in device_players:
-			player_prefixes_taken.erase(player.player_prefix)
-			player.queue_free()
-			
-		device_player_map.erase(device)
+	if connected:
+		add_player_input(device, joypad_movement_controls, "joy_left")
+		add_player_input(device, right_split_joypad_movement_controls, "joy_right")
+	elif not connected and device_player_map.has(device):
+		remove_players_of_device(device)
+		
+func remove_players_of_device(device: int):
+	var device_players = device_player_map[device] as Array[RigidBody2D]
+	for player in device_players:
+		player_prefixes_taken.erase(player.player_prefix)
+		player.queue_free()
+		
+	device_player_map.erase(device)
