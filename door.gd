3dead9740d2970e333bef33543aaf6237b0eea5c
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-05-10 18:09:02 +0200

    Make testing platform movement easier

--- door.gd
+++ door.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export var connected_plaform: AnimatableBody2D
 @export var move_difference: float = 300
 @export var move_duration: float = 3
+@export var activate_without_key: bool = true
 var tween: Tween
 
 func _on_area_entered(area: Area2D) -> void:
@@ -15,7 +16,7 @@ func _on_area_entered(area: Area2D) -> void:
 	button_visuals.show()
 
 func _on_button_body_entered(_body: Node2D) -> void:
-	if not button_visuals.visible or tween:
+	if (not activate_without_key and not button_visuals.visible) or tween:
 		return
 	button_visuals.play("pressed")
 		
