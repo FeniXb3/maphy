ac20ccb2e622294650113cf324afd508ee7c7c3f
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-06-07 09:12:48 +0200

    Show key connected with specific switch

--- info_container.gd
+++ info_container.gd
@@ -4,16 +4,18 @@ extends VBoxContainer
 @export var auto_joining_status_label: Label
 @export var joining_status_label: Label
 @export var split_joining_status_label: Label
+@export var map: Dictionary[Signal, Array]
 
 
 func _ready() -> void:
-	SignalBus.disabling_switched.connect(_on_switched.bind(disabling_status_label, "Disabling"))
-	SignalBus.joining_switched.connect(_on_switched.bind(joining_status_label, "Joining"))
-	SignalBus.split_joining_switched.connect(_on_switched.bind(split_joining_status_label, "Split joining"))
-	SignalBus.autojoining_switched.connect(_on_switched.bind(auto_joining_status_label, "Autojoining"))
+	SignalBus.disabling_switched.connect(_on_switched.bind(disabling_status_label, "Disabling", InputMap.action_get_events("disabling_switch")))
+	SignalBus.joining_switched.connect(_on_switched.bind(joining_status_label, "Joining", InputMap.action_get_events("joining_switch")))
+	SignalBus.split_joining_switched.connect(_on_switched.bind(split_joining_status_label, "Split joining", InputMap.action_get_events("split_joining_switch")))
+	SignalBus.autojoining_switched.connect(_on_switched.bind(auto_joining_status_label, "Autojoining", InputMap.action_get_events("autojoining_switch")))
 	
-func _on_switched(value: bool, label: Label, description) -> void:
-	label.text = "%s: %s" % [description, value]
+func _on_switched(value: bool, label: Label, description: String, events: Array[InputEvent]) -> void:
+	var key := events[0].as_text().replace(" (Physical)", "")
+	label.text = "%s [%s]: %s" % [description, key, value]
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_info"):
